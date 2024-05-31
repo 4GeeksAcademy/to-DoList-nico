ed5f623ad52ff282f5c1fd3797e17e598e6bbed9
Alvaro Nicolas Ortiz <160544793+ANicolasOrtiz@users.noreply.github.com>
2024-05-31 19:08:54 +0000

    ejercicio terminado

--- src/js/component/List.jsx
+++ src/js/component/List.jsx
@@ -25,19 +25,27 @@ const List = () => {
 
     return (
         <form className="container" onSubmit={(event) => addTodo(event)}>
-            <h1>To-Do List</h1>
+            <div>
+                <h1>To-Do List<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-check-square" viewBox="0 0 16 16">
+                    <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
+                    <path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z" />
+                </svg></h1>
+            </div>
             <input className="form-control form-control-lg" type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder="No tasks, please add a task" aria-label=".form-control-lg example"></input>
             <div className="todo-item">
                 <span className="todo-text"></span>
                 <button className="btn" type="submit">Add</button>
-                <ul>
-                    {list.map((todo) => (
-                        <li key={todo.id}>
-                            {todo.todo}
-                            <button className="btn btn-danger" onClick={() => deleteTodo(todo.id)}>&times;</button>
-                        </li>
-                    ))}
-                </ul>
+                <br></br>
+                <div>
+                    <ul>
+                        {list.map((todo) => (
+                            <li key={todo.id}>
+                                {todo.todo}
+                                <button className="btn btn-danger" onClick={() => deleteTodo(todo.id)}>&times;</button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </form>
     );
